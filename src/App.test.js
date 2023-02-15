513a58f77d2511ffa0321fcd2acbe9093976b9e6
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2023-02-15 23:06:24 +0900

    ⚡️ debug関数を実装

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+it("renders Hello", () => {
   render(<App />);
-  screen.debug();
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText("Hello");
+  screen.debug(element);
+  expect(element).toBeInTheDocument();
 });
